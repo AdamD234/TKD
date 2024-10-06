147a18ed62a8885871356b8cb9c3468371b85eaf
Adam Davies <120380228+AdamD234@users.noreply.github.com>
2024-10-06 10:14:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ if (document.cookie == ""){
   percentages = document.cookie.split(`; percentages=`).pop().split(';')[0].split(",");
 }
 
+if (percentages.includes(NaN)) {
+  for(i in percentages) {
+    if(isNaN(percentages[i])){
+      answeredQuestions[i] = 0;
+      correctQuestions[i] = 0;
+      percentages[i] = 0;
+    }
+  }
+  document.cookie = "answered=" + answeredQuestions.toString() + "; expires=Thu, 31 Oct 2024 12:00:00 UTC";
+  document.cookie = "correct=" + correctQuestions.toString() + "; expires=Thu, 31 Oct 2024 12:00:00 UTC";
+  document.cookie = "percentages=" + percentages.toString() + "; expires=Thu, 31 Oct 2024 12:00:00 UTC";
+}
+
 updateScreen();
 
 document.addEventListener("keyup", (event) => {
